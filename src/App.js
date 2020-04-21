8ca6f5b9390d6534c748fe16916f150831cfc741
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-04-21 16:16:26 +0700

    switch tag

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Navbar from "./components/navbar";
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from './components/home'
-import About from './components/about'
-import Contact from './components/contact'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Post from "./components/post";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 class Home extends Component {
   state = {
     posts: [],
@@ -10,7 +10,6 @@ class Home extends Component {
       this.setState({
         posts: res.data.slice(0, 10),
       });
-      console.log(this.state);
     });
   }
   render() {
@@ -20,7 +19,9 @@ class Home extends Component {
         return (
           <div className="post card" key={post.id}>
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={'/' + post.id}>
+                <span className="card-title">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
@@ -29,6 +30,7 @@ class Home extends Component {
     ) : (
       <div className="center">No posts yet</div>
     );
+
     return (
       <div className="container">
         <h4 className="center">Home</h4>
